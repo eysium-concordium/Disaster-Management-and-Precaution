562c21db9f5e9b0fe777f371a733952d9ba459b1
ProactiveCoder <manofiron639@gmail.com>
2023-09-16 19:48:08 +0530

    added navbar

---
+++ src/components/NavBar2/NavBar.js
@@ -0,0 +1,17 @@
+import React from "react";
+import './NavBar.css'
+
+function NavBar2(){
+    return(
+        <>
+        <div className="nav2">
+            <button>Home</button>
+            <button>Natural Disaster</button>
+            <button>Man-Made Disaster</button>
+            <button>Emergency</button>
+            <button>Maps</button>
+        </div>
+        </>
+    );
+}
+export default NavBar2;
--- src/index.js
+++ src/index.js
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import App from './App';
+import NavBar from './components/NavBar/NavBar';
+import NavBar2 from './components/NavBar2/NavBar';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <NavBar/>
+    <NavBar2/>
+    {/* <App /> */}
   </React.StrictMode>
 );
 
