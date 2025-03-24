d152719a4fcd252c21f7500b595958d83e93bfd6
aryanbroy <136257977+aryanbroy@users.noreply.github.com>
2025-03-24 16:40:46 +0530

    connect to database and setup env file

---
+++ server/db/index.js
@@ -0,0 +1,17 @@
+const { default: mongoose } = require("mongoose");
+
+const connectDB = async () => {
+  try {
+    const connectionInstance = await mongoose.connect(
+      `${process.env.DB_URI}/employee`
+    );
+    console.log(
+      `\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`
+    );
+  } catch (error) {
+    console.log("MONGODB connection FAILED ", error);
+    process.exit(1);
+  }
+};
+
+module.exports = { connectDB };
